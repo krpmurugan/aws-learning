4b553e80d5af18316ea374572421de58f476e2c6
Murugan <krpmurugan@gmail.com>
2024-03-22 16:56:41 +0530

    Add files via upload

--- golang/coin.go
+++ golang/coin.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+//Find minimum number of coins whose sum make the given amount. Coin values [1,5,10,50,100,500].  Example -  For 12 we need 3 coins, for 468 we need 10 coins.
+
 func findMinimumCoins(coins []int, target int) int {
 	count := 0
 	for i := len(coins) - 1; i >= 0; i-- {
