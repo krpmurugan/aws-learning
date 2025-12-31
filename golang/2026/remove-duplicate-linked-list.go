99fca9a598c43f88436d789d945e8fc6a095a027
Murugan <krpmurugan@gmail.com>
2025-12-31 22:03:42 +0530

    Create remove-duplicate-linked-list.go

---
+++ golang/2026/remove-duplicate-linked-list.go
@@ -0,0 +1,64 @@
+/*
+83. Remove Duplicates from Sorted List
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+
+I/P: 1 → 1 → 2 → 3 → 3
+O/p : 1->2->3 */
+
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+
+	for current != nil && current.Next != nil {
+		if current.Val == current.Next.Val {
+			current.Next = current.Next.Next // skip duplicate
+		} else {
+			current = current.Next
+		}
+	}
+
+	return head
+}
+
+func printList(n *ListNode) {
+	for n != nil {
+		fmt.Print(n.Val)
+		if n.Next != nil {
+			fmt.Print("-> ")
+		}
+		n = n.Next
+	}
+	fmt.Println()
+}
+
+func main() {
+	l1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val: 3,
+					},
+				},
+			},
+		},
+	}
+	res := deleteDuplicates(l1)
+	printList(res)
+
+}
