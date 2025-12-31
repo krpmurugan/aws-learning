c35cc1db2a207db8a90d3c9935b4552ce2b92bd7
Murugan <krpmurugan@gmail.com>
2025-12-31 21:49:18 +0530

    Create merge-linked-list.go

---
+++ golang/merge-linked-list.go
@@ -0,0 +1,55 @@
+/* Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4 */
+
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l1, l2.Next)
+	return l2
+}
+
+func printList(n *ListNode) {
+	for n != nil {
+		fmt.Print(n.Val)
+		if n.Next != nil {
+			fmt.Print(" -> ")
+		}
+		n = n.Next
+	}
+	fmt.Println()
+}
+
+func main() {
+	l1 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}
+	l2 := &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}
+	res := mergeTwoLists(l1, l2)
+	printList(res)
+}
